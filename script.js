6ce383c19d0444f2a15526c9d0c0529244dd43c0
Saurabh Pal <118064701+saurabh27pal@users.noreply.github.com>
2024-02-20 17:27:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
+
+function change_text(){
+    document.getElementById("demo").innerHTML="Embrace Sideboard with color and size samll added to cart !!!"
+}
